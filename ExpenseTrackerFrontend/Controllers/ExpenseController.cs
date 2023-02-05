0a94d1a3ced10dbee1ae9131990e102c1b419e95
Shivam Gupta <shivamsinghal357@gmail.com>
2023-02-05 13:33:56 +0530

    some more progress

--- ExpenseTrackerFrontend/Controllers/ExpenseController.cs
+++ ExpenseTrackerFrontend/Controllers/ExpenseController.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTrackerFrontend.Models.Domain;
+﻿using ExpenseTrackerFrontend.Models.CreateModel;
+using ExpenseTrackerFrontend.Models.Domain;
 using ExpenseTrackerFrontend.Views;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +8,16 @@ namespace ExpenseTrackerFrontend.Controllers
     [Route("expense")]
     public class ExpenseController : Controller  //one view foldeer per controller
     {
-        public IActionResult Index()
-        {
-            //It doesnot have any view with name 'Index' in View\Expense
-            //It will return error untill there is view with name 'Index'
-            return View();
+        //public IActionResult Index()
+        //{
+        //    //It doesnot have any view with name 'Index' in View\Expense
+        //    //It will return error untill there is view with name 'Index'
+        //    return View();
 
-        }
+        //}
 
         [HttpGet]
+        [Route("add")]
         public ViewResult AddExpense()  //name same as cshtml file (same name as view) -> one view folder per controller
         {
             ViewData["Title"] = "page from controller";
@@ -24,7 +26,8 @@ namespace ExpenseTrackerFrontend.Controllers
 
 
         [HttpPost]   //post on same route i.e. 'expense'
-        public async Task<IActionResult> AddExpense(Expense expense)
+        [Route("add")]
+        public async Task<IActionResult> AddExpense(CreateExpense expense)
         {
             //complete this function to backend call and redirect to ViewExpenses 
             //backend calls
@@ -39,7 +42,42 @@ namespace ExpenseTrackerFrontend.Controllers
 
         public async Task<IActionResult> ViewExpenses()
         {
-            return null;
+            var Expenses = new List<Expense>
+            {
+                new Expense
+                {
+                    Name = "Expense1",
+                    Amount = 20,
+                    Remarks = "Remark1",
+                },
+                new Expense
+                {
+                    Name = "Expense2",
+                    Amount = 20,
+                    Remarks = "Remark2",
+                },
+                new Expense
+                {
+                    Name = "Expense3",
+                    Amount = 20,
+                    Remarks = "Remark3",
+                },
+                new Expense
+                {
+                    Name = "Expense4",
+                    Amount = 20,
+                    Remarks = "Remark4",
+                },
+                new Expense
+                {
+                    Name = "Expense5",
+                    Amount = 20,
+                    Remarks = "Remark5",
+                }
+            };
+
+            ViewData["Expenses"] = Expenses; 
+            return View();
         }
     }
 }
